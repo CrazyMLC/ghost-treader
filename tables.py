692cb62950400d1429da48204a0519c1d990a0f1
CrazyMLC <crazymlc12@gmail.com>
2021-08-04 14:24:58 -0700

    Initial commit
    Large amount of work done on tables.py
    Small rewrite of text.py

--- tables.py
+++ tables.py
@@ -9,184 +9,273 @@ text_table = {
     0x07: '7',
     0x08: '8',
     0x09: '9',
-    0x0a: 'A',
-    0x0b: 'B',
-    0x0c: 'C',
-    0x0d: 'D',
-    0x0e: 'E',
-    0x0f: 'F',
-    0x10: 'G',
-    0x11: 'H',
-    0x12: 'I',
-    0x13: 'J',
-    0x14: 'K',
-    0x15: 'L',
-    0x16: 'M',
-    0x17: 'N',
-    0x18: 'O',
-    0x19: 'P',
-    0x1a: 'Q',
-    0x1b: 'R',
-    0x1c: 'S',
-    0x1d: 'T',
-    0x1e: 'U',
-    0x1f: 'V',
-    0x20: 'W',
-    0x21: 'X',
-    0x22: 'Y',
-    0x23: 'Z',
-    0x24: 'a',
-    0x25: 'b',
-    0x26: 'c',
-    0x27: 'd',
-    0x28: 'e',
-    0x29: 'f',
-    0x2a: 'g',
-    0x2b: 'h',
-    0x2c: 'i',
-    0x2d: 'j',
-    0x2e: 'k',
-    0x2f: 'l',
-    0x30: 'm',
-    0x31: 'n',
-    0x32: 'o',
-    0x33: 'p',
-    0x34: 'q',
-    0x35: 'r',
-    0x36: 's',
-    0x37: 't',
-    0x38: 'u',
-    0x39: 'v',
-    0x3a: 'w',
-    0x3b: 'x',
-    0x3c: 'y',
-    0x3d: 'z',
-    0x3e: '!',
-    0x3f: '?',
-    0x40: 'À',
-    0x41: 'Á',
-    0x42: 'Â',
-    0x43: 'Ä',
-    0x44: 'Æ',
-    0x45: 'Ç',
-    0x46: 'È',
-    0x47: 'É',
-    0x48: 'Ê',
-    0x49: 'Ë',
-    0x4a: 'Ì',
-    0x4b: 'Í',
-    0x4c: 'Î',
-    0x4d: 'Ï',
-    0x4e: 'Ñ',
-    0x4f: 'Ò',
-    0x50: 'Ó',
-    0x51: 'Ô',
-    0x52: 'Ö',
-    0x53: 'Œ',
-    # 0x54: Character looks blank, but isn't used as a space?
-    0x55: 'Ù',
-    0x56: 'Ú',
-    0x57: 'Û',
-    0x58: 'Ü',
-    0x59: 'à',
-    0x5a: 'á',
-    0x5b: 'â',
-    0x5c: 'ä',
-    0x5d: 'æ',
-    0x5e: 'ç',
-    0x5f: 'è',
-    0x60: 'é',
-    0x61: 'ê',
-    0x62: 'ë',
-    0x63: 'ì',
-    0x64: 'í',
-    0x65: 'î',
-    0x66: 'ï',
-    0x67: 'ñ',
-    0x68: 'ò',
-    0x69: 'ó',
-    0x6a: 'ô',
-    0x6b: 'ö',
-    0x6c: 'œ',
-    0x6d: 'ß',
-    0x6e: 'ù',
-    0x6f: 'ú',
-    0x70: 'û',
-    0x71: 'ü',
+	0x0a: 'A',
+	0x0b: 'B',
+	0x0c: 'C',
+	0x0d: 'D',
+	0x0e: 'E',
+	0x0f: 'F',
+	0x10: 'G',
+	0x11: 'H',
+	0x12: 'I',
+	0x13: 'J',
+	0x14: 'K',
+	0x15: 'L',
+	0x16: 'M',
+	0x17: 'N',
+	0x18: 'O',
+	0x19: 'P',
+	0x1a: 'Q',
+	0x1b: 'R',
+	0x1c: 'S',
+	0x1d: 'T',
+	0x1e: 'U',
+	0x1f: 'V',
+	0x20: 'W',
+	0x21: 'X',
+	0x22: 'Y',
+	0x23: 'Z',
+	0x24: 'a',
+	0x25: 'b',
+	0x26: 'c',
+	0x27: 'd',
+	0x28: 'e',
+	0x29: 'f',
+	0x2a: 'g',
+	0x2b: 'h',
+	0x2c: 'i',
+	0x2d: 'j',
+	0x2e: 'k',
+	0x2f: 'l',
+	0x30: 'm',
+	0x31: 'n',
+	0x32: 'o',
+	0x33: 'p',
+	0x34: 'q',
+	0x35: 'r',
+	0x36: 's',
+	0x37: 't',
+	0x38: 'u',
+	0x39: 'v',
+	0x3a: 'w',
+	0x3b: 'x',
+	0x3c: 'y',
+	0x3d: 'z',
+	0x3e: '!',
+	0x3f: '?',
+	0x40: 'À',
+	0x41: 'Á',
+	0x42: 'Â',
+	0x43: 'Ä',
+	0x44: 'Æ',
+	0x45: 'Ç',
+	0x46: 'È',
+	0x47: 'É',
+	0x48: 'Ê',
+	0x49: 'Ë',
+	0x4a: 'Ì',
+	0x4b: 'Í',
+	0x4c: 'Î',
+	0x4d: 'Ï',
+	0x4e: 'Ñ',
+	0x4f: 'Ò',
+	0x50: 'Ó',
+	0x51: 'Ô',
+	0x52: 'Ö',
+	0x53: 'Œ',
+	# 0x54: Character looks blank, but isn't used as a space?
+	0x55: 'Ù',
+	0x56: 'Ú',
+	0x57: 'Û',
+	0x58: 'Ü',
+	0x59: 'à',
+	0x5a: 'á',
+	0x5b: 'â',
+	0x5c: 'ä',
+	0x5d: 'æ',
+	0x5e: 'ç',
+	0x5f: 'è',
+	0x60: 'é',
+	0x61: 'ê',
+	0x62: 'ë',
+	0x63: 'ì',
+	0x64: 'í',
+	0x65: 'î',
+	0x66: 'ï',
+	0x67: 'ñ',
+	0x68: 'ò',
+	0x69: 'ó',
+	0x6a: 'ô',
+	0x6b: 'ö',
+	0x6c: 'œ',
+	0x6d: 'ß',
+	0x6e: 'ù',
+	0x6f: 'ú',
+	0x70: 'û',
+	0x71: 'ü',
 
-    0x73: '¡',
-    0x74: '¿',
+	0x73: '¡',
+	0x74: '¿',
 
-    0xe1: '.',
+	0xe1: '.',
 
-    0xe5: '(',
-    0xe6: ')',
+	0xe5: '(',
+	0xe6: ')',
 
-    0xe9: '“',  # Not used?
-    0xea: '”',  # Curly but used for all quotation marks anyway
+	0xe9: '“',  # Not used?
+	0xea: '”',  # Curly but used for all quotation marks anyway
 
-    0xed: ':',
+	0xed: ':',
 
-    0xef: ',',
+	0xef: ',',
 
-    0xf2: '*',
-    0xf3: '’',
-    0xf4: '-',
+	0xf2: '*',
+	0xf3: '’',
+	0xf4: '-',
 
-    0xf7: '%',
-    0xf8: '…',  # XXX Vertically centered
-    0xf9: '~',
-    0xfa: '«',  # XXX Bigger
-    0xfb: '»',  # XXX Bigger
-    0xfc: '&',
-    0xfd: '☆',
-    0xfe: '♪',
-    0xff: ' ',
+	0xf7: '%',
+	0xf8: '…',  # XXX Vertically centered
+	0xf9: '~',
+	0xfa: '«',  # XXX Bigger
+	0xfb: '»',  # XXX Bigger
+	0xfc: '&',
+	0xfd: '☆',
+	0xfe: '♪',
+	0xff: ' ',
 
-    # XXX Obtain images, move out of tables
-    0x0116: '[[BACK]]',
-    0x0117: '[[TRICK]]',
-    0x0118: '[[GHOST]]',
+	# XXX Obtain images, move out of tables
+	0x0116: '[[BACK]]',
+	0x0117: '[[TRICK]]',
+	0x0118: '[[GHOST]]',
 }
 
 colors = {
-    0x6: 'RED',
-    0x9: 'BLUE',
-    0xf: 'BLACK',
+	0x6: 'RED',
+	0x9: 'BLUE',
+	0xf: 'BLACK',
 }
 
 portraits = {
-    0x01: 'Jeego',
+	0x01: 'Jeego',
 
-    0x07: 'Lynne_sweating',
+	0x04: 'Lynne_smile',
+	0x07: 'Lynne_sweat',
+	0x0d: 'Lynne_pensive',
+	0x0e: 'Lynne_think',
+	0x13: 'Lynne_frown',
+	0x14: 'Lynne_serious',
+	0x16: 'Lynne_surprise',
+	0x1c: 'Lynne_pensive',
 
-    0x0d: 'Lynne_pensive',
+	0x27: 'Sissel_smile',
+	0x29: 'Sissel_frown',
+	0x2b: 'Sissel_pensive',
+	0x2d: 'Sissel_sweat',
+	0x2f: 'Sissel_befuddled',
+	0x34: 'Sissel_shrug',
+	0x36: 'Sissel_surprise',
+	0x3a: 'Sissel_angry',
+	0x44: 'Sissel_empty',#plays sissel's portrait noise on text boxes
 
-    0x13: 'Lynne_frown',
+	0x72: 'Sith_left',
 
-    0x27: 'Sissel_smile',
+	0x7b: 'Jeego_left',
+	0x7c: 'Jeego_right',
+	
+	0x7f: 'Tengo_right',
 
-    0x29: 'Sissel_frown',
+	0x80: 'Ray_on_left',
+	0x81: 'Ray_on_right',
+	0x83: 'Ray_off_right',
+	0x85: 'Ray_soul',
+	0x86: 'Ray_empty',#plays ray's portrait noise on text boxes
+	
+	0x88: 'Robot_right',
+	
+	0x8a: 'Cabanela_',
+	
+	0xc6: 'Green_Detective',
+	
+	0xca: 'Blue_Detective',
+	
+	0xcc: 'Blue_Doctor',
+	
+	0xee: 'Card_Guard',
+	
+	0xea: 'Bailey',
+	0xec: 'Bailey_empty',
 
-    0x2b: 'Sissel_pensive',
+	0x10c: 'Floating-soul',
+}
 
-    0x2d: 'Sissel_sweating',
+mini_portraits = {
+	0x1: 'Sissel',
+	0x2: 'Missile',
+	0x3: 'Lynne',
+	0x4: 'Ray',
+	0x5: 'None',
+	0x6: 'None2',#??
+	0x7: 'Guardian',
+	0x8: 'Cat'
+}
 
-    0x2f: 'Sissel_befuddled',
+sounds = {
+	0x00: 'silence',
+	
+	0x15: 'surprise',
+	0x16: 'intrigue',
+	0x17: 'yell',
+	0x18: 'slam'
+}
 
-    0x34: 'Sissel_shrug',
+commands = {
+	0xff01: '\n',
+	0xff02: '[CLEAR]\n\n',#waits for input, then clears the textbox.
+	0xff03: '[CENTER]',#centers text printed afterwards, until a new line. produced undesirable results when used in the middle of a line.
+	0xff04: '[SPEED {0}]',#text speed, higher seems slower. frames per character?
+	0xff05: '[COLOR {0}]',#changes text color
+	0xff06: '[SHOW]',#shows the textbox
+	0xff07: '[HIDE]',#hides the textbox
+	0xff08: '[PORTRAIT {0}]\n',#portraits have different barks on appearing
+	
+	0xff0c: '[PAUSE {0}]',#pauses text output. measured in frames?
+	0xff0d: '[SFX {0}]',#sound effects
+	
+	0xff0f: '[FLASH]',#flashes the screen white
+	0xff10: '[SHAKE {0} FOR {{0}}]',#time measured in frames?
+	
+	0xff15: '[CLEAR IN {0}]\n\n',#clears the textbox after some time. measured in frames?
+	0xff16: '[BREAK]\n\n',#breaks up narrator text. softlocks when used in a text box.
+	
+	0xff19: '[PORTRAIT {0} FADE {{0}}]',#lower FADE is slower. no portrait barks?
+	
+	0xff1b: '[MINI_PORTRAIT {0}]\n',#mini portraits on the side of dialogue boxes
+	0xff1c: '[MUSIC {0}]',#changes music. could use another table.
+	
+	0xfffe: '[STOP]',#ends a scene
+	
+	0xff20: '[LOWER {0}]',#drops text down, more goes lower.
+	0xff21: '[SCRIPTED_PAUSE]',#pauses until a scripted condition is met. not available on all dialogue. does not clear the textbox.
+	0xff22: '[APPEAR {0}]',#affects fade in speed of narrator text. since it's used to accelerate fade in speed rather than slow it down, i'll just call it 'appear'. could use more testing, effects of the number may not be linear.
+	0xff23: '[SKIP]',#attempts to skip remaining text, doesn't pause on [STOP]
+	0xff24: '[FADE IN {0}]',#fades text in all at once.
+	
+	0xff28: '[VOLUME {0}]',#changes music volume
+	
+	0xff2d: '[START Event {0}, Scene {{0}}]\n',#Event # seems to roughly correlate with story progress. Scene # seems to be sequential within each event, usually. some files contain scenes from multiple events... as of now, unsure how the game uses this information or if it even matters.
+}
 
-    0x36: 'Sissel_surprised',
-
-    0x3a: 'Sissel_angry',
-
-    0x7b: 'Jeego_left',
-    0x7c: 'Jeego_right',
-
-    0x80: 'Ray_on_left',
-    0x81: 'Ray_on_right',
-
-    0x83: 'Ray_off_right',
-
-    0x10c: 'Floating-soul',
+stages = {#where is the park???? where is yomiel's death puzzle??
+	'st01': "Junkyard",
+	'st02': "Super's Office",
+	'st03': "Guard Room",#Is everything in the prison in this folder? Is the Moonlit Courtyard here??
+	'st04': "Kitchen",
+	'st05': "Novelist's Apt.",
+	'st06': "Lynne's Apartment",
+	'st07': "Kamila's Old House",
+	'st09': "Minister's Office",
+	'st11': "Special Investigation",
+	'st13': "The Chicken Kitchen",
+	'st14': "Luxurious Parlor",
+	'st15': "Epilogue"
 }
--- text.py
+++ text.py
@@ -3,96 +3,104 @@ from io import BytesIO
 from struct import unpack
 from sys import argv
 
-from lzss3 import decompress
 from tables import *
 
+
+
 def decode_message(message_data):
-    """Decode the message from an iterator"""
+	"""Decode the message from an iterator"""
 
-    message = ''
+	message = ''
 
-    for char in message_data:
-        if char == 0xff01:
-            message += '\n'
-        elif char == 0xff02:
-            message += '\n\n'
-        elif char == 0xff05:
-            color = next(message_data)
-            color = colors.setdefault(color, '#' + str(color))
-            message += '[[COLOR {0}]]'.format(color)
-        elif char == 0xff08:
-            portrait = next(message_data)
-            portrait = portraits.setdefault(portrait, '#' + hex(portrait))
-            message += '[[PORTRAIT: {0}]]\n'.format(portrait)
-        else:
-            message += text_table.setdefault(char, r'\x{0:04x}'.format(char))
+	for char in message_data:
+		if char in commands:
+			command = commands[char]
+			parameters = []
+			for i in range(command.count('{0')):
+				parameters.append(next(message_data))
+			for param in parameters:
+				p = param
+				if command.count('COLOR {'):
+					p = colors.setdefault(param, r'!!!!!\x{0:04x}'.format(param))
+				elif command.count('MINI_PORTRAIT {'):
+					p = mini_portraits.setdefault(param, r'!!!!!\x{0:04x}'.format(param))
+				elif command.count('PORTRAIT {'):
+					p = portraits.setdefault(param, r'!!!!!\x{0:04x}'.format(param))
+				elif command.count('SFX {'):
+					p = sounds.setdefault(param, r'!!!!!\x{0:04x}'.format(param))
+				command = command.format(p)
+			message += command
+			if command.count('[STOP]'):
+				break
+		else:
+			message += text_table.setdefault(char, r'!!!!!\x{0:04x}'.format(char))
 
-    return message
+	#print(message)
+	return message
 
 def get_label(data, pointer):
-    """Get and return the label at the given pointer."""
-    data.seek(pointer)
-    label = bytearray(b'')
+	"""Get and return the label at the given pointer."""
+	data.seek(pointer)
+	label = bytearray(b'')
 
-    while True:
-        char, = data.read(1)
-        if char == 0:
-            break
-        else:
-            label.append(char)
+	while True:
+		char, = data.read(1)
+		if char == 0:
+			break
+		else:
+			label.append(char)
 
-    return label.decode('ASCII')
+	return label.decode('ASCII')
 
 def message_iterator(data, pointer):
-    """Yield the message at the given pointer, character by character.
+	"""Yield the message at the given pointer, character by character.
 
-    Each character is two bytes long and little endian.
-    """
-    data.seek(pointer)
-    while True:
-        char, = unpack('<H', data.read(2))
+	Each character is two bytes long and little endian.
+	"""
+	data.seek(pointer)
+	while True:
+		char, = unpack('<H', data.read(2))
 
-        if char == 0xfffe:
-            raise StopIteration
-        else:
-            yield char
+		#if char == 0xfffe:
+		#	raise StopIteration
+	   # else:
+		yield char
 
 
 with open(argv[1], 'rb') as text_file:
-    data = decompress(text_file)
-
+	data = text_file.read()
 data = BytesIO(data)
+
 
 assert data.read(4) == b'1LMG'
 
 # Seek to and read message pointers
 data.seek(8)
 pointers_offset, = unpack('<H', data.read(2))
-data.seek(pointers_offset + 0x34)
+data.seek(pointers_offset + 0x38)
 
-assert data.read(4) == b'\x2a\x00\x00\x00'  # Not sure of the significance
+#assert data.read(4) == b'\x2a\x00\x00\x00'  # Not sure of the significance
 
 message_count, = unpack('<L', data.read(4))
 messages = []
 Message = namedtuple('Message', ['label_offset', 'message_pointer'])
 
 for m in range(message_count):
-    messages.append(
-        Message( *unpack('<LL', data.read(8)) )
-    )
+	messages.append(
+		Message( *unpack('<LL', data.read(8)) )
+	)
 
 labels_pointer = data.tell()  # XXX Can I actually *find* this anywhere?
 
 for message in messages:
-    # Find the label and print it as a header
-    label = get_label(data, labels_pointer + message.label_offset)
+	# Find the label and print it as a header
+	label = get_label(data, labels_pointer + message.label_offset)
 
-    print(label)
-    print('=' * len(label))
+	print(label, message.message_pointer)
+	print('=' * len(label))
 
+	# Extract the actual message
+	message = message_iterator(data, message.message_pointer)
+	message = decode_message(message)
 
-    # Extract the actual message
-    message = message_iterator(data, message.message_pointer)
-    message = decode_message(message)
-
-    print(message, end='\n\n')
+	print(message, end='\n\n')
