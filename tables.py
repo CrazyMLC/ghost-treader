f759475e6bf4524ee05cedbf3a5514703a5fc36f
CrazyMLC <crazymlc12@gmail.com>
2021-08-30 12:17:45 -0700

    Portraits complete
    some function renamed as well, according to suggestions

--- tables.py
+++ tables.py
@@ -157,58 +157,210 @@ colors = {
 	0xf: 'BLACK',
 }
 
-portraits = {
+portraits = {# Every portrait has a flipped version, except the 'empty' potraits. Flipped portraits are always right after the unflipped portrait.
+	#0x00: 'Broken',
+	
 	0x01: 'Jeego',
-
-	0x04: 'Lynne_smile',
+	#0x02: 'Jeego_r',
+	0x03: 'Lynne_smile',
+	0x05: 'Lynne_happy',
 	0x07: 'Lynne_sweat',
-	0x0d: 'Lynne_pensive',
-	0x0e: 'Lynne_think',
-	0x13: 'Lynne_frown',
-	0x14: 'Lynne_serious',
-	0x16: 'Lynne_surprise',
-	0x1c: 'Lynne_pensive',
-
+	0x09: 'Lynne_sad',
+	0x0b: 'Lynne_shout',
+	0x0d: 'Lynne_think',
+	0x0f: 'Lynne_angry',#shout, but with a fist
+	0x11: 'Lynne_proud',
+	0x13: 'Lynne_serious',
+	0x15: 'Lynne_surprise',
+	0x17: 'Lynne_fear',
+	0x19: 'Lynne_shock',
+	0x1b: 'Lynne_pensive',
+	0x1d: 'Lynne_empty',
+	
+	0x1e: 'KidLynne_sad1',#hat,stick
+	0x20: 'KidLynne_sad2',#stick
+	0x22: 'KidLynne_sad3',#only sadness
+	0x24: 'KidLynne_cat',
+	0x26: 'KidLynne_empty',
+	
 	0x27: 'Sissel_smile',
 	0x29: 'Sissel_frown',
-	0x2b: 'Sissel_pensive',
+	0x2b: 'Sissel_think',
 	0x2d: 'Sissel_sweat',
 	0x2f: 'Sissel_befuddled',
+	0x31: 'Sissel_ghost',
+	0x33: 'Sissel_empty2',####
 	0x34: 'Sissel_shrug',
 	0x36: 'Sissel_surprise',
-	0x3a: 'Sissel_angry',
-	0x44: 'Sissel_empty',#plays sissel's portrait noise on text boxes
-
-	0x72: 'Sith_left',
-
-	0x7b: 'Jeego_left',
-	0x7c: 'Jeego_right',
+	0x38: 'Sissel_pensive',
+	0x3a: 'Sissel_shout',
+	0x3c: 'Sissel_cat',
+	0x3e: 'Sissel_empty3',####
+	0x3f: 'Yomiel_machine',
+	0x41: 'Yomiel_empty',
+	0x42: 'Yomiel_evil',
+	0x44: 'Sissel_empty',####The first(only?) empty that sissel uses
+	0x45: 'Yomiel_smile',
+	0x47: 'Yomiel_frown',
+	0x49: 'Yomiel_think',
+	0x4b: 'Yomiel_sweat',
+	0x4d: 'Yomiel_befuddled',
+	0x4f: 'Yomiel_shrug',
+	0x51: 'Yomiel_surprise',
+	0x53: 'Yomiel_pensive',
+	0x55: 'Yomiel_shout',
+	0x57: 'Yomiel_evil2',
+	0x59: 'Yomiel_empty',#####
+	0x5a: 'Sissel_smile2',
+	0x5c: 'Sissel_peek',
 	
-	0x7f: 'Tengo_right',
-
-	0x80: 'Ray_on_left',
-	0x81: 'Ray_on_right',
-	0x83: 'Ray_off_right',
-	0x85: 'Ray_soul',
+	0x5e: 'Emma_smile',
+	0x60: 'Emma_shock',
+	0x62: 'Emma_empty',
+	
+	0x63: 'Kamila_smile',
+	0x65: 'Kamila_think',
+	0x67: 'Kamila_happy',
+	0x69: 'Kamila_possessed',
+	0x6b: 'Kamila_cry',
+	0x6d: 'Kamila_sad',
+	0x6f: 'Kamila_shout',
+	0x71: 'Kamila_empty',
+	
+	0x72: 'Sith',
+	0x74: 'Sith_empty',
+	
+	0x75: 'Missile',
+	0x77: 'Missile_old',
+	0x79: 'Missile_empty',
+	0x7a: 'Missile_empty2',
+	
+	0x7b: 'Jeego2',
+	0x7d: 'Jeego_empty',
+	
+	0x7e: 'Tengo',
+	
+	0x80: 'Ray_on',
+	0x82: 'Ray_off',
+	0x84: 'Ray_soul',#when ray speaks over a soul
 	0x86: 'Ray_empty',#plays ray's portrait noise on text boxes
 	
-	0x88: 'Robot_right',
+	0x88: 'Robot',
 	
-	0x8a: 'Cabanela_',
+	0x89: 'Cabanela_smile',
+	0x8b: 'Cabanela_empty',#?
+	0x8c: 'Cabanela_happy',
+	0x8e: 'Cabanela_sweat',
+	0x90: 'Cabanela_pensive',
+	0x92: 'Cabanela_shout',
+	0x94: 'Cabanela_frown',
 	
-	0xc6: 'Green_Detective',
+	0x96: 'Jowd_serious',
+	0x98: 'Jowd_frown',
+	0x9a: 'Jowd_laugh',
+	0x9c: 'Jowd_smile',
+	0x9e: 'Jowd_shout',
+	0xa0: 'Jowd_serious_past',
+	0xa2: 'Jowd_frown_past',
+	0xa4: 'Jowd_smile_past',
+	0xa6: 'Jowd_serious_jacket',
+	0xa8: 'Jowd_frown_jacket',
+	0xaa: 'Jowd_smile_jacket',
+	0xac: 'Jowd_smug',
+	0xae: 'Jowd_pensive',
+	0xb0: 'Jowd_laugh_past',
+	0xb2: 'Jowd_shout_past',
+	0xb4: 'Jowd_smug_past',
+	0xb6: 'Jowd_laugh_jacket',
+	0xb8: 'Jowd_shout_jacket',
+	0xba: 'Jowd_smug_jacket',
+	0xbc: 'Jowd_empty',
 	
-	0xca: 'Blue_Detective',
+	0xbd: 'Officer_serious',
+	0xbf: 'Officer_serious2',#?
+	0xc1: 'Officer_devastated',
+	0xc3: 'Officer_empty',
 	
-	0xcc: 'Blue_Doctor',
+	0xc4: 'Officer2',#looks unfamiliar?
 	
-	0xee: 'Card_Guard',
+	0xc6: 'GreenDetective',
+	0xc8: 'GreenDetective_empty',
 	
-	0xea: 'Bailey',
+	0xc9: 'BlueDetective',
+	
+	0xcb: 'BlueDoctor',
+	
+	0xcd: 'Rindge',
+	0xcf: 'Rindge_empty',
+	
+	0xd0: 'Memry',
+	
+	0xd2: 'PidgeonMan',
+	0xd4: 'PidgeonMan_empty',
+	
+	0xd5: 'Dandy',
+	0xd7: 'Dandy_empty',
+	
+	0xd8: 'Beauty',
+	0xda: 'Beauty_empty',
+	
+	0xdb: 'Bartender',
+	
+	0xdd: 'Amelie_upset',
+	0xdf: 'Amelie_smile',
+	0xe1: 'Amelie_healthy',
+	
+	0xe3: 'Minister_tense',
+	0xe5: 'Minister_shock',
+	0xe7: 'Minister_smile',
+	0xe9: 'Minister_empty',
+	
+	0xea: 'Bailey_serious',
 	0xec: 'Bailey_empty',
+	
+	0xee: 'CardGuard',
+	
+	0xef: 'ArmedGuard',
+	
+	0xf1: 'Officer3',
+	0xf3: 'Officer3_empty',
+	
+	0xf4: 'Rocker',
+	
+	0xf6: 'CurryLover',
+	
+	0xf8: 'Guardian',
+	0xfa: 'Guardian_preach',
+	0xfc: 'Guardian_empty',
+	
+	0xfd: 'Chef',
+	0xff: 'Chef_empty',
+	
+	0x100: 'PoliceChief',
+	0x102: 'PoliceChief_empty',
+	
+	0x103: 'MinisterGuard',
+	
+	0x105: 'YonoaOfficer',
+	
+	0x107: 'Alma_smile',
+	0x109: 'Alma_happy',
 
-	0x10c: 'Floating-soul',
+	0x10b: 'Soul',
+	0x10d: 'Soul_empty',
+	
+	#0x10e: 'Message'
+	#0x10f: crash
 }
+def fill_in_portraits():
+	temp = []
+	for key,item in portraits.items():
+		if key+1 not in portraits:
+			temp.append(key+1)
+	for key in temp:
+		portraits[key] = portraits[key-1]+"_r"
+fill_in_portraits()
+
 
 mini_portraits = {
 	0x1: 'Sissel',
@@ -231,8 +383,8 @@ sounds = {
 }
 
 commands = {
-	0xff01: '[NEW]\n',#making this into a [] code, so that \n characters can be ignored by the encoder and used solely to make text files easier to read. hard choice, but i think this makes everything less finnicky overall. should also help the whole newline changing with OS problem?
-	0xff02: '[CLEAR]\n\n',#waits for input, then clears the textbox.
+	0xff01: '[BREAK]\n',#making this into a [] code, so that \n characters can be ignored by the encoder.
+	0xff02: '[WAIT]\n\n',#waits for input, then clears the textbox.
 	0xff03: '[CENTER]',#centers text printed afterwards, until a new line. produced undesirable results when used in the middle of a line.
 	0xff04: '[SPEED {0}]',#text speed, higher seems slower. frames per character?
 	0xff05: '[COLOR {0}]',#changes text color
@@ -247,7 +399,7 @@ commands = {
 	0xff10: '[SHAKE {0} FOR {{0}}]',#time measured in frames?
 	
 	0xff15: '[CLEAR IN {0}]\n\n',#clears the textbox after some time. measured in frames?
-	0xff16: '[BREAK]\n\n',#breaks up narrator text. softlocks when used in a text box.
+	0xff16: '[BREAK_NARRATOR]\n\n',#breaks up narrator text. softlocks when used in a text box.
 	
 	0xff19: '[FADE_PORTRAIT {0} IN {{0}}]\n',#lower FADE is slower. no portrait barks?
 	
